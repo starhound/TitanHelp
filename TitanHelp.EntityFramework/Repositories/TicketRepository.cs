d182251a405988bc158c544c63fc75f17eb6d107
starhound <wesleyreid0@gmail.com>
2022-10-06 12:15:11 -0400

    ef new ticket command

---
+++ TitanHelp.EntityFramework/Repositories/TicketRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TitanHelp.Data.Models;
+using TitanHelp.EntityFramework.Data;
+
+namespace TitanHelp.EntityFramework.Repositories
+{
+    internal class TicketRepository
+    {
+        public TicketContext _dbContext { get; set; }
+        public TicketRepository(TicketContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool WriteNewTicketCommand(Ticket newTicket)
+        {
+            _dbContext.Add(newTicket);
+            _dbContext.SaveChanges();
+            return true;
+        }
+    }
+}
